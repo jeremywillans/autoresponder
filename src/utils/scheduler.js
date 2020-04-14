1cfd18eb3491a7cdfbfed508329ce2e2189980b6
Jeremy Willans <Jeremy.Willans@empired.com>
2020-04-14 12:49:16 +1000

    Fix PrimaryEnabled

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -32,7 +32,7 @@ schedule.scheduleJob(tokenRefresh, async (fireDate) => {
         refresh_token: accessCodes.refresh_token,
         primaryId: output.primaryId || '',
         primaryEmail: output.primaryEmail || '',
-        primaryEnabled: output.primaryEmail || '',
+        primaryEnabled: output.primaryEnabled || '',
         autoResponse: output.autoResponse || '',
         mentionAll: output.mentionAll || '',
         supressionTime: output.supressionTime || '',
